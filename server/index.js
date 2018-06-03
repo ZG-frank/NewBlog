03aeebd654d11e5e88ec4de0f19a0f36ef097226
ZG-frank <zzgwhu2015@163.com>
2018-06-03 23:55:55 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
-const Koa = require('koa')
+const Koa = require('koa');
+const config = require('./configs');
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+const api = require('./api');
+
+const Router = require('koa-router');
+// const routerInfo = require('koa-router')();
+
 const App = new Koa();
+const router = new Router();
+
+mongoose.connect(config.mongodb.url, config.mongodbSecret);
+mongoose.connection.on('error', console.error);
+mongoose.connect('mongodb://127.0.0.1/Blog');
+
 
 const first = (Context) => {
     if (Context.request.path === '/'){
@@ -8,8 +22,22 @@ const first = (Context) => {
 	if (Context.request.path === '/admin'){
 		Context.response.body = '<p>admin-frankzgzhou</p>';
 	}
-
 };
 
-App.use(first);
-App.listen(8080);
+// console.log(api,api())
+// App.use(api());
+// App.use(routerInfo.routes());
+// App.use(first);
+
+// router.get('/', (ctx, next) => {
+// 	ctx.response.body = '<h1>hello Koa</h1>';
+// });
+App.use(bodyParser());
+App
+    .use(api.routes())
+    .use(api.allowedMethods());
+
+// create server
+App.listen(config.app.port, () => {
+    console.log('The server is running at http://localhost:' + config.app.port);
+});
