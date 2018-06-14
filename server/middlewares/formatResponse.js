d343363f755f01264601686597634686c0149804
ZG-frank <zzgwhu2015@163.com>
2018-06-14 23:57:15 +0800

    update

--- server/middlewares/formatResponse.js
+++ server/middlewares/formatResponse.js
@@ -8,4 +8,13 @@ formatResponse = (ctx, data) => {
     };
 }
 
+responseError = (ctx, data) => {
+    ctx.throw(402, JSON.stringify({
+        code: data.code,
+        message: data.message,
+        data: data.data,
+        space: data.space
+    }));
+}
+
 module.exports = formatResponse;
