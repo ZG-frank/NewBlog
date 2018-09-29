52977f8e7a4d9bb747bee1063309243166fabf65
ZG-frank <zzgwhu2015@163.com>
2018-09-29 22:52:03 +0800

    [update] babel config

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,19 +1,18 @@
 const presets = [
     ["@babel/env", {
-        "targets": {
-            "browsers": ["last 2 versions", "safari >= 7"]
+        targets: {
+            browsers: ["last 2 versions", "safari >= 7"]
         },
-        "modules": false
+        modules: false
     }],
     ["@babel/react"]
   ];
 
 const plugins = [
-    ["import", { 
-        "libraryName": "antd",
-        "style": "css"
-    }],
-    ["@babel/plugin-proposal-class-properties"]
+    ["import", { libraryName: 'antd', style: true }],
+    ["@babel/plugin-proposal-class-properties"],
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-syntax-dynamic-import"]
 ];
   
 module.exports = { presets, plugins };
