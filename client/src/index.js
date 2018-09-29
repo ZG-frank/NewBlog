3881c66f0f56e0d67eaba16e7c51d534f2d872c8
ZG-frank <zzgwhu2015@163.com>
2018-09-29 22:55:12 +0800

    [update] index.html

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Dropdown, Menu } from 'antd';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Routers from './routers';
+import store from './store';
 import './index.less';
 
-const menu = (
-    <Menu>
-        <Menu.Item>
-            <a target="_blank">1st menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank">2nd menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank">3rd menu item</a>
-        </Menu.Item>
-    </Menu>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routers />
+                </BrowserRouter>
+            </Provider>
+        )
+    }
+}
 
-ReactDOM.render(
-    <div className="content">
-        <Dropdown overlay={menu} trigger={['click']}>
-            <Button>Hello, world!</Button>
-        </Dropdown>
-    </div>
-    ,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
