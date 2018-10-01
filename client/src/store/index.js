e761f93967bb53e5c839badc6443f2a7dd97b850
ZG-frank <zzgwhu2015@163.com>
2018-10-02 01:11:04 +0800

    [update] store config

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import reducers from './reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 const store = createStore(
-    reducers,
+    rootReducers,
     composeEnhancers(
-        applyMiddleware(thunk)
+        applyMiddleware(...middlewares)
     )
 )
 
