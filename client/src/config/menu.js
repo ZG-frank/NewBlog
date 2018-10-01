cd412de589352c5a40ba807dbf4dc31059559886
ZG-frank <zzgwhu2015@163.com>
2018-10-02 01:07:36 +0800

    [update] menu config

--- client/src/config/menu.js
+++ client/src/config/menu.js
@@ -1,71 +1,24 @@
-export default
-[{
-    key   : '/home',
-    title : '首页',
-    icon  : 'home'
-},{
-    key   : '/moduleManage',
-    title : '模块管理',
-    icon  : 'mokuai',
-    list  : [{
-        key   : '/moduleList',
-        title : '模块列表'
-    }]
+export default [{
+    key   : '/dashboard',
+    title : 'Dashboard',
+    icon  : 'dashboard'
 }, {
-    key   : '/functionManage',
-    title : '功能管理',
-    icon  : 'gongneng',
-    list  : [{
-        key   : '/functionList',
-        title : '功能列表'
-    }]
-}, {
-    key   : '/roleManage',
-    title : '角色管理',
-    icon  : 'biaoshilei_jiaosepeizhi',
-    list  : [{
-        key   : '/roleList',
-        title : '角色列表'
-    }]
-}, {
-    key   : '/employeesManage',
-    title : '员工管理',
-    icon  : 'yuangongguanli',
-    list  : [{
-        key   : '/employeesList',
-        title : '员工列表'
-    }]
+    key   : '/postManage',
+    title : 'Posts',
+    icon  : 'read'
 }, {
     key   : '/userManage',
-    title : '用户管理',
-    icon  : 'yonghu',
+    title : 'Users',
+    icon  : 'team',
     list  : [{
         key   : '/userList',
         title : '用户列表'
     }, {
-        key   : '/userOrder',
-        title : '用户订单'
-    }, {
-        key   : '/userManager',
+        key   : '/userEdit',
         title : '用户管理'
-    }, {
-        key   : '/endOrder',
-        title : '结束订单'
     }]
 }, {
-    key   : '/visualManage',
-    title : '可视化管理',
-    icon  : 'yanjing',
-    list  : [{
-        key   : '/visualList',
-        title : '可视化列表'
-    }]
-}, {
-    key   : '/priceManage',
-    title : '价格配置管理',
-    icon  : 'wuliaojiageleixing',
-    list  : [{
-        key   : '/priceConfig',
-        title : '价格配置'
-    }]
+    key   : '/tagManage',
+    title : 'Tags',
+    icon  : 'read'
 }]
--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,50 +1,26 @@
 import Loadable from 'react-loadable'
 import DelayLoading from '../components/DelayLoading'
 
-const Home         = Loadable({loader: () => import('../views/Home'), loading : DelayLoading,delay:3000})
-const ModuleList   = Loadable({loader: () => import('../views/ModuleManage/ModuleList'), loading : DelayLoading,delay:3000})
-const FunctionList = Loadable({loader: () => import('../views/FunctionManage/FunctionList'), loading : DelayLoading,delay:3000})
-const RoleList     = Loadable({loader: () => import('../views/RoleManage/RoleList'), loading : DelayLoading,delay:3000})
-const EmployeesList= Loadable({loader: () => import('../views/EmployeesManage/EmployeesList'), loading : DelayLoading,delay:3000})
-const UserList     = Loadable({loader: () => import('../views/UserManage/UserList'), loading : DelayLoading,delay:3000})
-const UserOrder    = Loadable({loader: () => import('../views/UserManage/UserOrder'), loading : DelayLoading,delay:3000})
-const UserManager  = Loadable({loader: () => import('../views/UserManage/UserManager'), loading : DelayLoading,delay:3000})
-const EndOrder     = Loadable({loader: () => import('../views/UserManage/EndOrder'), loading : DelayLoading,delay:3000})
-const VisualList   = Loadable({loader: () => import('../views/VisualManage/VisualList'), loading : DelayLoading,delay:3000})
-const PriceConfig  = Loadable({loader: () => import('../views/PriceManage/PriceConfig'), loading : DelayLoading,delay:3000})
+const Dashboard  = Loadable({loader: () => import('../views/Admin/Dashboard'), loading : DelayLoading,delay:3000})
+const UserList   = Loadable({loader: () => import('../views/Admin/UserManage/UserList'), loading : DelayLoading,delay:3000})
+const UserEdit   = Loadable({loader: () => import('../views/Admin/UserManage/UserEdit'), loading : DelayLoading,delay:3000})
+const postManage = Loadable({loader: () => import('../views/Admin/postManage'), loading : DelayLoading,delay:3000})
+const tagManage  = Loadable({loader: () => import('../views/Admin/tagManage'), loading : DelayLoading,delay:3000})
 
 export default
 [{
-    'path': '/home',
-    'component': Home
-}, {
-    'path': '/moduleManage/moduleList',
-    'component': ModuleList
-}, {
-    'path': '/functionManage/functionList',
-    'component': FunctionList
-}, {
-    'path': '/roleManage/roleList',
-    'component': RoleList
-}, {
-    'path': '/employeesManage/employeesList',
-    'component': EmployeesList
+    'path': '/dashboard',
+    'component': Dashboard
 }, {
     'path': '/userManage/userList',
     'component': UserList
 }, {
-    'path': '/userManage/userOrder',
-    'component': UserOrder
+    'path': '/userManage/userEdit',
+    'component': UserEdit
 }, {
-    'path': '/userManage/userManager',
-    'component': UserManager
+    'path': '/postManage',
+    'component': postManage
 }, {
-    'path': '/userManage/endOrder',
-    'component': EndOrder
-}, {
-    'path': '/visualManage/visualList',
-    'component': VisualList
-}, {
-    'path': '/priceManage/priceConfig',
-    'component': PriceConfig
+    'path': '/tagManage',
+    'component': tagManage
 }]
